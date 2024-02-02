f7172c51f109f616e65f3041ebf79c331a0816c9
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-02 18:29:35 +0100

    snack4 - risultato a pagina

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -8,42 +8,29 @@ let numberArray = [];
 // prompt per il numero a 4 cifre
 let number = prompt("Inserire numero a 4 cifre:");
 
+// dichiaro variabile somma
+let sum = 0;
 
 // controllo che il numero sia a 4 cifre
 if (!isNaN(number) && number.length === 4) {
-    // dichiaro variabile somma
-    let sum = 0;
     
     // inserire i numeri nell'array con .push
     numberArray.push(number);  
     
-    let digit1 = Math.floor(numberArray / 1000);
-    let digit2 = Math.floor((numberArray % 1000) / 100);
-    let digit3 = Math.floor((numberArray % 100) / 10);
-    let digit4 = number % 10;
+    let digit1 = Math.floor(number[0]);
+    let digit2 = Math.floor(number[1]);
+    let digit3 = Math.floor(number[2]);
+    let digit4 = Math.floor(number[3]);
 
     
     // somma 
     sum = digit1 + digit2 + digit3 + digit4
-    
-    
-    
-    
-    
-    // log
-console.log(sum);
-console.log(numberArray);
 
-
-
-
-    
-   
 }else {
     // se non è a 4 cifre
     alert("error");
 }
 
 
-
+document.querySelector(".sum").innerHTML = `il tuo numero dà come somma di ogni cifra: ` + sum;
  
