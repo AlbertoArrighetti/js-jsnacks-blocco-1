cbcacd6f84e1dce53bc758187b579a5c3316e7da
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-02 16:14:41 +0100

    snack2 - ARRAY e ciclo FOR

---
+++ jsnack2/script.js
@@ -0,0 +1,31 @@
+// Il software deve chiedere per 10 volte
+// all’utente di inserire un numero.
+
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// creo un array per contenere i numeri inseriti
+let numberArray = [];
+// dichiaro già la somma
+let sum = 0;
+
+// creare un ciclo FOR che chiede il numero 10 volte
+// per ora tramite prompt
+for (let i = 0; i < 10; i++) {
+    
+    // inserisco il prompt per chiedere i numeri
+    const numbers = Number(prompt(`inserisci il ${i + 1}° numero da sommare`));
+
+    // variabile per ottenere la somma
+    sum = sum + numbers;
+    // inserire i numeri nell'array con .push
+    numberArray.push(numbers);
+    
+    console.log(numberArray);
+    console.log("numero :" + sum);
+
+
+}
+
+
+
+console.log(sum);
