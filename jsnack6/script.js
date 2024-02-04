475c3feca66371b6c92bc714090805a10c2482e1
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-04 17:44:54 +0100

    snack6 - stampa in pagina

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -4,23 +4,21 @@
 
 // dichiaro una variabile per la somma
 let sum = 0;
-
 // dichiaro una variabile per la media
 let avarage = 0;
+
 
 // creo un ciclo FOR per generare i primi 10 numeri (da 1 a 10)
 for (i = 1; i <= 10; i++){
 
     // somma
     sum += i;
-
-    
-    // log
-    console.log(i, sum);
 }
-
 
 // media
 avarage = sum / 10;
 
-console.log("media", avarage);
+
+// stampa elementi in pagina
+document.querySelector(".numbers-sum").innerHTML = "La somma dei numeri da 1 a 10 è: " + sum;
+document.querySelector(".numbers-avarage").innerHTML = "La media è: " + avarage;
