07a9c36749af44ad1c1b65927becd6a3827620f5
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-04 16:38:35 +0100

    snack5 - ciclo for con numeri al cubo in console

---
+++ jsnack5/script.js
@@ -0,0 +1,27 @@
+// Stampa il cubo dei primi N numeri,
+// dove N è un numero indicato dall’utente.
+
+// (partendo da 1, il numero N indicato dall'utente è compreso)
+
+// utente inserisce un numero
+const userNumber = prompt("Inserisci un numero.");
+// creo un array che contenga il numero dell'user
+const userNumberArray = [userNumber];
+
+
+// creo un ciclo che mi dia tutti i numeri fino a quello inserito 
+for (i = 0; i <= userNumberArray; i++){
+    
+    
+    const cubeCalc = (i * i * i);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    console.log(cubeCalc);
+}
