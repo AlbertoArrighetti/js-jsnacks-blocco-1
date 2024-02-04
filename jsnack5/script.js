413754597c07fb4f3593a5c414eaa489e8f9e17c
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-04 16:54:07 +0100

    snack5 - stampa in pagina

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -3,25 +3,25 @@
 
 // (partendo da 1, il numero N indicato dall'utente è compreso)
 
+
+
 // utente inserisce un numero
 const userNumber = prompt("Inserisci un numero.");
-// creo un array che contenga il numero dell'user
+// creo un array che contenga il numero dell'utente
 const userNumberArray = [userNumber];
 
 
 // creo un ciclo che mi dia tutti i numeri fino a quello inserito 
-for (i = 0; i <= userNumberArray; i++){
+for (i = 1; i <= userNumberArray; i++){
+    // numeri elevati al cubo
+    let cubeCalc = (i * i * i);
     
     
-    const cubeCalc = (i * i * i);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    console.log(cubeCalc);
+    // stampa dei numeri
+    document.querySelector(".user-numbers").innerHTML = "il numero da te inserito è: " + userNumber;
+    // stampa numeri elevati al cubo
+    document.querySelector(".cube-numbers").innerHTML += "numero " + i + " = " + cubeCalc + "<br>" ;
+  
+    // log
+    // console.log(cubeCalc);
 }
